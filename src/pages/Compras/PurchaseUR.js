ace9d1615302bd3fffb0c7690ac6e9b897d0f9f0
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-04-18 21:41:23 -0600

    boton
    trest

--- src/pages/Compras/PurchaseUR.js
+++ src/pages/Compras/PurchaseUR.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MasterPage from "../../components/MasterPage";
 import NavTabMenu from "../../components/NavTabMenu";
+import { Button } from "react-bootstrap";
 
 export default function PurchaseUR() {
     return (
@@ -8,6 +9,7 @@ export default function PurchaseUR() {
             <MasterPage/>
             <NavTabMenu/>
             <h1>Aqui deben de ir todos los libros pero aún no lo hago xd</h1>
+            <Button>Ola bb</Button>
         </>
     )
 }
