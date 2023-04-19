01ddb93fe6887c4fa2f4db40afd60dd215beaea9
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-04-18 22:50:28 -0600

    Update Compras
    Buttons

--- src/views/Compras/PurchaseUR.js
+++ src/views/Compras/PurchaseUR.js
@@ -8,8 +8,18 @@ export default function PurchaseUR() {
         <>
             <MasterPage/>
             <NavTabMenu/>
-            <h1>Aqui deben de ir todos los libros pero aún no lo hago xd</h1>
-            <Button>Ola bb</Button>
+            <div style={{ width: "100%", padding: 50, paddingLeft: 10, alignItems: "center"}}>
+            <h1>Visualizar</h1>
+            <div style={{paddingLeft: 30}}>
+            <Button variant = "info" size="lg" style={{ padding: 15}}>Pedidos</Button>  
+            </div>
+           
+            </div>
+            
+            <div style={{ width: "100%", padding: 50, paddingLeft: 150 }} >
+            <h1>Visualizar</h1>
+            <Button variant = "info" size="lg">Proveedores</Button>  
+            </div>
         </>
     )
 }
