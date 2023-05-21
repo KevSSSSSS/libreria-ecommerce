f5a63e404d5c81d9683d1f8fdf97a1140ad8886b
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-05-21 04:35:24 -0600

    Actualización de titulo
    Se añadio la funcion de ver el nombre del usuario

--- src/views/Compras/PurchaseHome.js
+++ src/views/Compras/PurchaseHome.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import MasterPage from "../../components/MasterPage";
 import NavTabMenu from "../../components/NavTabMenu";
 import { Button } from "react-bootstrap";
+import { UserContext } from "../../models/UserContext";
 
 export default function PurchaseHome() {
+    const { user } = useContext(UserContext);
     return (
         <>
             <MasterPage/>
             <NavTabMenu/>
-            <h1 style={{marginLeft:600}}>Bienvenido @user</h1>
+            <h1 style={{marginLeft:600}}>Bienvenido {user.nombre}</h1>
             <div style={{marginTop: 100, marginLeft: 300, marginRight: 150, border: "solid"}}>   
             <h1>Productos: </h1>
                 <div style={{padding: 100}}>
