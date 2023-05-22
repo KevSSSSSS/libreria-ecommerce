e9b1380b9162f8c155bba6ecd64b5c2e80067a77
juan gil <jgill@toluca.tecnm.mx>
2023-05-22 12:12:55 -0600

    1
    -se agregó una vista sobre que no hay resultados
    -se corrigió una falta de ortografía

---
+++ src/views/ClienteAnonimo/NoRes.js
@@ -0,0 +1,20 @@
+import MasterPage from "../../components/MasterPage";
+import NavTabMenu from "../../components/NavTabMenu";
+import CardBooks from "../../components/Card";
+import BannerHome from "../../components/BannerHome";
+
+
+
+export default function NoRes (){
+
+    return(
+        <>
+            <MasterPage />
+            <NavTabMenu />
+            <BannerHome /> 
+        </>
+
+    )
+
+
+}
