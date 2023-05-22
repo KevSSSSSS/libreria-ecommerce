37c847e9c08603cd50f4092025e488d56965257c
Ruben <ruben.glu@outlook.com>
2023-05-22 13:37:01 -0600

    Update EnvioOrden.js

--- src/views/Paqueteria/EnvioOrden.js
+++ src/views/Paqueteria/EnvioOrden.js
@@ -2,9 +2,8 @@ import React, { useContext, useState } from "react";
 import { Button, Form, Image, Spinner } from "react-bootstrap";
 
 import { baseUrlAPI, colors, fontFamily } from "../../constants/constants";
-import fondo from "../../assets/background1.jpg";
 import { Link, useNavigate } from "react-router-dom";
-import { UserContext } from "../../models/UserContext";
+
 
 
 export default function EnvioOrden() {
@@ -12,31 +11,44 @@ export default function EnvioOrden() {
     return (
         <>
         <div style={{ textAlign: "center", display: "flex", alignItems: "center", height: "100vh", justifyContent: "center", backgroundImage: `url(https://images.pexels.com/photos/2908984/pexels-photo-2908984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`, backgroundSize: "cover", backgroundColor: `rgb(40,40,40)`, backgroundBlendMode: "soft-light",}}>
-            <div style={{ width: "50%", height: "60vh", backgroundColor: colors.white, borderRadius: 20, alignItems: "center", justifyContent: "center", display: "flex" }}>
+            <div style={{ width: "50%", height: "80vh", backgroundColor: colors.white, borderRadius: 20, alignItems: "center", justifyContent: "center", display: "flex" }}>
                 <div style={{ color: colors.terceary }}>
-                    <h4 style={{ fontFamily: fontFamily.primary, fontSize: "30px"}}>Envío de Orden</h4>
+                    <h4 style={{ fontFamily: fontFamily.primary, fontSize: "35px", marginTop: "5px"}}>Envío de Orden</h4>
+                    <h4 style={{ fontFamily: fontFamily.primary, fontSize: "25px"}}>ID: </h4>
                     <Form style={{ marginTop: 30 }} >
                         <Form.Group style={{ marginBottom: 20 }}>
-                            <Form.Label>Usuario</Form.Label>
-                            <Form.Control type="email" name="email" placeholder="Introduzca su email" ></Form.Control>
+                            <Form.Label>Paqueteria: </Form.Label>
+                            <Form.Control type="text" name="nombre" placeholder="Introduzca el nombre de la paqueteria" ></Form.Control>
+                        </Form.Group>
+                        <Form.Group style={{ marginBottom: 20 }}>
+                            <Form.Label>No. Guia: </Form.Label>
+                            <Form.Control type="text" name="guia" placeholder="Introduzca el numero de guia" ></Form.Control>
+                        </Form.Group>
+                        <Form.Group style={{ marginBottom: 20 }}> 
+                            <Form.Label>Fecha de Envio: </Form.Label>
+                            <Form.Control type="text" name="fech_env" placeholder="Introduzca la fecha de envio" ></Form.Control>
                         </Form.Group>
                         <Form.Group>
-                            <Form.Label>Contraseña</Form.Label>
-                            <Form.Control type="password" name="contrasena" placeholder="Introduzca su contraseña" ></Form.Control>
+                            <Form.Label>Fecha de Entrega: </Form.Label>
+                            <div style={{ display: "flex", flexDirection: "row" , alignItems: "center", justifyContent: "center" }}>
+                            <Form.Control type="text" name="dia_fech_ent" placeholder="DD"  style={{width: "8vh"}}></Form.Control>
+                            <h3 style={{marginRight: "5px", marginLeft: "5px"}}>/</h3>
+                            <Form.Control type="text" name="mes_fech_ent" placeholder="MM" style={{width: "8vh"}}></Form.Control>
+                            <h3 style={{marginRight: "5px", marginLeft: "5px"}}>/</h3>
+                            <Form.Control type="text" name="año_fech_ent" placeholder="AAAA" style={{width: "8vh"}}></Form.Control>
+                            </div>
                         </Form.Group>
-                        <div style={{ color: "red", marginTop: 8 }}>Los dos campos son requeridos.</div>
-                        <div style={{ color: "red", marginTop: 8 }}>El correo o contraseña están incorrectos.</div>
-                        <div style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}><Spinner animation="grow" variant="warning" /></div>
-                        <Button style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginTop: 20 }}  type="submit">
-                            Entrar
+                        <div style={{ width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}></div>
+                        <Button size="lg" style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginTop: 80, marginRight: 80 }}  type="submit">
+                            Confirmar
                         </Button>
-                    </Form>
-                    <div style={{ marginTop: 20 }}>¿Aún no tienes una cuenta? Registrate gratis</div>
-                    <Link to={"/register"}>
-                        <Button style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginTop: 5 }} >
-                            Registrarse
+                        <Link to={"/ordenesnoenv"}>
+                        <Button size="lg" style={{ backgroundColor: colors.primary, borderColor: colors.primary, marginTop: 80 }} >
+                            Cancelar
                         </Button>
                     </Link>
+                    </Form>
+                    
                 </div>
             </div>
         </div>
