404987fbe212cb652431beb05aa889d3940ed1ad
juan gil <jgill@toluca.tecnm.mx>
2023-05-23 00:14:38 -0600

    autor

--- src/views/Home.js
+++ src/views/Home.js
@@ -17,9 +17,47 @@ export default function Home() {
     const [books, setBooks] = useState([]);
 
     const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState("");
+    const handleChange = (event)=>
+    {
+        const { name, value } = event.target;
+     setSearch(value)
+    }
+
+    const onSubmit= ()=>
+        {
+            console.log(search);
+        onSearch(search)
+        }
+
+    const onSearch = (search) => {
+  console.log('aki si entra');
+        if (search !== "") {
+
+            let tempData = books.filter((item) => {
+
+                return (
+
+                    item.autor.toLowerCase().indexOf(search.toLowerCase()) > -1
+
+
+                );
+
+            });
+
+       setBooks(tempData);
+
+        } else {
+
+            
+
+        }
+
+    };
 
     useEffect(() => {
         setLoading(true);
+        console.log('aki si entra');
         fetch(`${baseUrlAPI}libros`)
             .then(response => response.json())
             .then((data) => {
@@ -32,9 +70,24 @@ export default function Home() {
             })
     }, []);
 
+
     return (
         <>
-            <MasterPage />
+         <div style={{ width: "100%", height: "15vh", padding: 50, alignItems: "center", justifyContent: "space-between", display: "flex", backgroundImage: `url(https://imgs.search.brave.com/wxDaGP_cjgg6zCtRAV82KWAVCQhKdBI3XOkLnlynIiw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDIwMzY4/OTcuanBn)` }}>
+            <Image style={{ width: "6%" }} src="https://imgs.search.brave.com/ubuMxJsB94CNuWODv8lg-4lkoBvkm2xZos4nK7kKKkg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/bG9nb2x5bnguY29t/L2ltYWdlcy9sb2dv/bHlueC8wOS8wOTZk/ZWM1NTI1NTUxYTEw/NjAzZDIzMmRiNWMx/NjY1MS5wbmc"></Image>
+            <Form className="d-flex" style={{ width: "30%" }}>
+                <Form.Control
+                    type="search"
+                    name= "Barra"
+                    placeholder="Buscar libros por nombre y autor"
+                    className="me-2"
+                    aria-label="Search"
+                    onChange={handleChange}
+                />
+                <Button variant="light" onClick={onSubmit}>Buscar</Button>
+            </Form>
+        </div>
+        {/*<MasterPage Busqueda={onSearch}/>*/}
             <NavTabMenu />
             <BannerHome />
             <div style={{ backgroundColor: "#ebebeb", height: "40vh" }}>
