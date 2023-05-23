dce574e1360e20728efd8080efb5c82f276441f0
Ruben <ruben.glu@outlook.com>
2023-05-22 18:57:35 -0600

    de nuevo

--- src/App.js
+++ src/App.js
@@ -17,18 +17,14 @@ import Payment from './views/ClienteRegistrado/Payment';
 import Account from './views/Devoluciones/Account';
 
 
-//Importaciones de Ordenes
+//Importaciones de Paqueteria
 import HomePaq from './views/Paqueteria/HomePaqueteria';
 import OrdenesNoEnviadas from './views/Paqueteria/OrdenesNoEnviadas';
 import EnvioOrden from './views/Paqueteria/EnvioOrden';
 import EntregaOrden from './views/Paqueteria/EntregaOrden';
 import OrdenesEnviadas from './views/Paqueteria/OrdenesEnviadas';
+import viewPaq from './views/Paqueteria/CrudsPaqueterias/viewPaqueteria';
 
-//Importaciones de Paqueterias
-import viewPaqueteria from './views/Paqueteria/Paqueterias/viewPaqueteria';
-import upPaqueteria from './views/Paqueteria/Paqueterias/upPaqueteria';
-import modPaqueteria from './views/Paqueteria/Paqueterias/modPaqueteria';
-import delPaqueteria from './views/Paqueteria/Paqueterias/delPaqueteria';
 
 
 import NoRes from './views/ClienteAnonimo/NoRes';
@@ -101,14 +97,13 @@ function App() {
           <Route path='/viewproducts' element={<ViewProduct />} />
         //End routes for Compras
 
-        //Rutas para Ordenes
+        //Rutas para paqueteria
         <Route path='/homepaq' element={<HomePaq />} />
         <Route path='/ordenesnoenv' element={<OrdenesNoEnviadas />} />
         <Route path='/envioorden' element={<EnvioOrden />} />
         <Route path='/ordenesenv' element={<OrdenesEnviadas />} />
         <Route path='/entregaorden' element={<EntregaOrden />} />
-
-        <Route path='/viewpaq' element={<viewPaqueteria/>}/>
+        <Route path='/viewpaq' element={<viewPaq />} />
 
 
         //Routes for Customer Support Client
