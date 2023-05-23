ac65a9f4686caf1fcb95377d00e23024d4acaba3
Ruben <ruben.glu@outlook.com>
2023-05-22 20:20:13 -0600

    Update ViewPaqueterias.js

--- src/views/Paqueteria/ViewPaqueterias.js
+++ src/views/Paqueteria/ViewPaqueterias.js
@@ -12,7 +12,7 @@ export default function ViewPaqueterias() {
         <>
             <BannerPaqueteria />
             <TabMenuPaqueterias />
-            <div style={{backgroundImage: 'url(https://img.interempresas.net/fotos/3253005.jpeg)', overflow: "hidden", height: "auto", maxWidth: "100%", backgroundSize: "cover", backgroundPosition:"center", backgroundRepeat: "no-repeat", backgroundColor: `rgb(80,80,80)`, backgroundBlendMode: "soft-light"}}>
+            <div style={{backgroundImage: 'url(https://img.interempresas.net/fotos/3253005.jpeg)', overflow: "hidden", height: "auto", maxWidth: "100%", backgroundSize: "cover", backgroundPosition:"center", backgroundRepeat: "no-repeat", backgroundColor: `rgb(100,100,100)`, backgroundBlendMode: "soft-light"}}>
             <div style={{ height: "15vh" , display: "flex"}}>
                 <form style={{ width: "20%" , height: "35%", margin: "1%", display: "flex", marginTop: "5vh"}}>
                     <label for="texto" style={{width: "50%", margin: "2%", color: "white", fontSize: "25px"}}>Buscar:</label>
@@ -25,19 +25,15 @@ export default function ViewPaqueterias() {
                 <table style={{backgroundColor: colors.blackSwift, width: "100%", verticalAlign: "center", border: "1px solid #fba71b", margin: "3vh"}}>
                     <tr style={{textAlign: "center", border: "1px solid #fba71b", height: "8%", background: colors.primary}}>
                         <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>ID</th>
-                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Direccion</th>
-                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Detalle de Orden</th>
-                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Enviar Orden</th>
-                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Cancelar Orden</th>
+                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Nombre</th>
+                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Editar</th>
+                        <th style={{border: "1px solid #fba71b", fontSize: "25px", color: "white"}}>Eliminar</th>
                     </tr>
                     <tr style={{fontSize: "20px", textAlign: "center", border: "1px solid #e1b683", height: "12%", background: colors.white}}>
                         <td>1</td>
-                        <td>Tec toluca</td>
+                        <td>UPS</td>
                         <td>
                         <Button variant="secondary" >ver detalle</Button>
-                        </td>
-                        <td>
-                            <Button variant="secondary" >Enviar</Button>
                         </td>
                         <td>
                             <Button variant="outline-danger" >X</Button>
